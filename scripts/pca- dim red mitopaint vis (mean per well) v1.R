35a0f1643755f16b11d892acc9459e02c7a54673
sarahnjfranks <snjfranks@gmail.com>
2026-08-04 16:45:44 +0100

    update pca-dim red mitopaint vis (mean per well) v1 to work using plot_pca function and to have fixed legend spacing

--- scripts/pca- dim red mitopaint vis (mean per well) v1.R
+++ scripts/pca- dim red mitopaint vis (mean per well) v1.R
@@ -13,241 +13,256 @@ library(ggpubr)
 library(tidyverse)
 library(viridis)
 library(ggrepel)
-# set file variables ####
+library(cowplot)
+# set variables ####
 file_name <- "mPaintDR2_N2_N3_N4"
 redu_state <- "redu"
 integrate_state <- "integrated"
-# load file ####
-# load data as df
-df <- as.data.frame(
-  fread(
-    paste(
-      "data/processed/", file_name, "_", integrate_state ,"_", redu_state, "_pca_embeddings.csv", sep = ""), 
-    header = TRUE)
-)
-# keep rownames as WELL_BATCH
-rownames(df) <- df$V1
-df$V1 <- NULL
-# load metadata as meta
-meta <- as.data.frame(
-  fread(
-    paste(
-      "data/processed/", file_name, "_", integrate_state, "_", redu_state, "_dimred_meta.csv", sep = ""), 
-    header = TRUE)
-)
-# keep rownames as WELL_BATCH
-rownames(meta) <- meta$V1
-meta$V1 <- NULL
-# load pca variance as var
-var <- as.data.frame(
-  fread(
-    paste(
-      "data/processed/", file_name, "_", integrate_state ,"_", redu_state, "_pca_var.csv", sep = ""), 
-    header = TRUE)
-)
-# keep rownames as WELL_BATCH
-rownames(var) <- var$V1
-var$V1 <- NULL
-# set plot variables ####
-# plotting variables
-x_lab <- paste0(
-  "PC_1 (",
-  round(var[1, "Percent_Variance"], 2),
-  "%)"
-)
-y_lab <- paste0(
-  "PC_2 (",
-  round(var[2, "Percent_Variance"], 2),
-  "%)"
-)
-size_title <- 10
-size_axis <- 8
-size_point <- 2
-plot_width <- 4
-plot_height <- 3.2
 pastel_cols <- lighten(c("#440154FF", "#238A8DFF", "#FDE725FF"), amount = 0.3)
-# p1: pc1 x pc2 by compound ####
-# make plots container to hold plots
-plots <- list()
-# make data cbind of meta and df
-data <- cbind(meta, df)
-# reorder plot data if only treated with DMSO, CCCP, ROT
-if (all(unique(meta$Compound) %in% c("DMSO", "CCCP", "ROT"))) {
-  data <- data %>%
-    mutate(
-      Compound = factor(
-        Compound,
-        levels = c("DMSO", "CCCP", "ROT")
-      )
-    )
-} else {
-  data <- data
+# create function to load data ####
+load_data <- function(file_name,
+                      integrate_state,
+                      redu_state) {
+  # load PCA embeddings
+  pca_embeddings <- as.data.frame(
+    fread(paste0("data/processed/",file_name,"_",integrate_state,
+                 "_",redu_state,"_pca_embeddings.csv"),
+      header = TRUE))
+  rownames(pca_embeddings) <- pca_embeddings$V1
+  pca_embeddings$V1 <- NULL
+  # load metadata
+  meta <- as.data.frame(
+    fread(paste0("data/processed/",file_name,"_",integrate_state,
+                 "_",redu_state,"_dimred_meta.csv"),
+      header = TRUE))
+  rownames(meta) <- meta$V1
+  meta$V1 <- NULL
+  # load PCA var
+  pca_var <- as.data.frame(
+    fread(paste0("data/processed/",file_name,"_",integrate_state,
+        "_",redu_state,"_pca_var.csv"),
+      header = TRUE))
+  pca_var$V1 <- NULL
+  # keep rows aligned between meta and PCA embeddings
+  meta <- meta[rownames(pca_embeddings), , drop = FALSE]
+  return(list(
+      pca_embeddings = pca_embeddings,
+      meta = meta,
+      pca_var = pca_var))
 }
-# plot is p1
-plots$p1 <- ggplot(data,aes(x = PC_1, y = PC_2)
-                   ) +
-  geom_point(aes(color = Compound), size = size_point) +
-  labs(title = "PCA by Compound",
-       x = x_lab,
-       y = y_lab) +
-  theme_pubr() +
-  theme(
-    plot.title = element_text(hjust = 0.5, size = size_title, face = "bold"),
-    axis.text = element_text(size = size_axis),
-    axis.title = element_text(size = size_axis),
-    legend.title = element_text(size = size_axis),
-    legend.text = element_text(size = size_axis),
-    panel.grid = element_blank(),
-    plot.margin = margin(10, 10, 10, 10)
-  ) +
-  scale_color_manual(
+# load data ####
+data <- load_data(
+  file_name = file_name,
+  integrate_state = integrate_state,
+  redu_state = redu_state
+)
+# create function to plot pca ####
+# axis labels
+x_lab <- paste0("PC_1 (",round(data$pca_var$Percent_Variance[1],2),"%)")
+y_lab <- paste0("PC_2 (",round(data$pca_var$Percent_Variance[2],2),"%)")
+
+plot_pca <- function(data,
+                     colour_var,
+                     title_text,
+                     colour_scale,
+                     shape_var = NULL,
+                     legend_title = NULL) {
+  # combine embeddings and meta in plot_df
+  plot_df <- data$pca_embeddings |>
+    cbind(data$meta)
+  # reorder compounds when appropriate
+  if (all(unique(plot_df$Compound) %in% c("DMSO", "CCCP", "ROT"))) {
+    plot_df$Compound <- factor(
+      plot_df$Compound,
+      levels = c("DMSO", "CCCP", "ROT")
+    )
+  }
+  # plot pca (aes)
+  p <- ggplot(plot_df,aes(x = PC_1,y = PC_2,
+                     colour = .data[[colour_var]]))
+  # plot pca (shape)
+  if (is.null(shape_var)) {
+    p <- p +
+      geom_point(size = 2)
+  } else {
+    p <- p +
+      geom_point(aes(shape = .data[[shape_var]]),
+        size = 2)
+  }
+  # plot pca (theme)
+  p +
+    colour_scale +
+    labs(
+      title = title_text,
+      colour = legend_title,
+      shape = shape_var,
+      x = x_lab,
+      y = y_lab
+    ) +
+    # tidy theme
+    theme_pubr() +
+    theme(
+      aspect.ratio = 1,
+      plot.title = element_text(
+        hjust = 0.5,
+        size = 9,
+        face = "bold"
+      ),
+      axis.text = element_text(size = 7),
+      axis.title = element_text(size = 7),
+      legend.title = element_text(size = 8),
+      legend.text = element_text(size = 7),
+      legend.position = "right",
+      panel.grid = element_blank()
+    )
+}
+# p1: pc1 x pc2 by compound ####
+# initialise plots list
+plots <- list()
+plots$pca_compound <- plot_pca(
+  data,
+  colour_var = "Compound",
+  title_text = "PCA by Compound",
+  legend_title = "Compound",
+  colour_scale = scale_colour_manual(
     values = pastel_cols
-    ) + 
-  theme(legend.position = "right")
-plots$p1 
+  )
+)
+plots$pca_compound
 # p2: pc1 x pc2 by concentration ####
-plots$p2 <- ggplot(data, aes(x = PC_1, y = PC_2)) +
-  geom_point(aes(color = Concentration), size = size_point) +
-  labs(title = "PCA by Concentration",
-       x = x_lab,
-       y = y_lab) +
-  theme_pubr() +
-  theme(
-    plot.title = element_text(hjust = 0.5, size = size_title, face = "bold"),
-    axis.text = element_text(size = size_axis),
-    axis.title = element_text(size = size_axis),
-    legend.title = element_text(size = size_axis),
-    legend.text = element_text(size = size_axis),
-    panel.grid = element_blank(),
-    plot.margin = margin(10, 10, 10, 10)
-  ) +
-  scale_color_gradientn(
-    colours = rev(lighten(
-      viridis(100),
-      amount = 0.3
-    ))
-  ) +
-  theme(legend.position = "right")
-plots$p2
+plots$pca_concentration <- plot_pca(
+  data,
+  colour_var = "Concentration",
+  title_text = "PCA by Concentration",
+  legend_title = "Concentration (μM)",
+  colour_scale = scale_colour_gradientn(
+    colours = rev(lighten(viridis(100),
+        amount = 0.3))
+  )
+)
+plots$pca_concentration
 # p3: pc1 x pc2 by pca nn ####
-labels <- data %>%
-  group_by(PCA_NN) %>%
+# assign floating labels positions for NN
+plot_df <- data$pca_embeddings |>
+  cbind(data$meta)
+labels <- plot_df |>
+  group_by(PCA_NN) |>
   summarise(
     PC_1 = median(PC_1),
     PC_2 = median(PC_2),
     .groups = "drop"
   )
-plots$p3 <- ggplot(data, aes(x = PC_1, y = PC_2)) +
-  geom_point(aes(color = as.factor(PCA_NN)), size = size_point) +
-  labs(title = "PCA by PCA NN",
-       x = x_lab,
-       y = y_lab,
-       color = "PCA_NN") +
-  theme_pubr() +
-  theme(
-    plot.title = element_text(hjust = 0.5, size = size_title, face = "bold"),
-    axis.text = element_text(size = size_axis),
-    axis.title = element_text(size = size_axis),
-    legend.title = element_text(size = size_axis),
-    legend.text = element_text(size = size_axis),
-    panel.grid = element_blank(),
-    plot.margin = margin(10, 10, 10, 10)
+plots$pca_nn <- ggplot(
+  plot_df,aes(x = PC_1,y = PC_2,
+              colour = as.factor(PCA_NN))
   ) +
+  geom_point(size = 2) +
   geom_label_repel(
     data = labels,
-    aes(
-      x = PC_1,
-      y = PC_2,
-      label = PCA_NN,
-      color = as.factor(PCA_NN)   # match label color to group
-    ),
+    aes(x = PC_1,y = PC_2,label = PCA_NN,
+      colour = as.factor(PCA_NN)),
     fill = "white",
     size = 3,
-    label.size = 0.35,
+    linewidth = 0.35,
     fontface = "bold",
     box.padding = 0.4,
     point.padding = 0.3,
     max.overlaps = Inf,
-    segment.color = NA,
+    segment.colour = NA,
     show.legend = FALSE
   ) +
-  theme(legend.position = "right")
-plots$p3
+  labs(title = "PCA by PCA NN",
+      colour = "PCA_NN",
+      x = x_lab,
+      y = y_lab) +
+  # tidy theme
+  theme_pubr() +
+  theme(
+    aspect.ratio = 1,
+    plot.title = element_text(
+      hjust = 0.5,
+      size = 9,
+      face = "bold"
+    ),
+    axis.text = element_text(size = 7),
+    axis.title = element_text(size = 7),
+    legend.title = element_text(size = 8),
+    legend.text = element_text(size = 7),
+    legend.position = "right",
+    panel.grid = element_blank()
+  )
+plots$pca_nn
 # p4: pc1 x pc1 by compound and concentration ####
-plots$p4 <- ggplot(data,aes(x = PC_1, y = PC_2)) +
-  geom_point(aes(color = Concentration, shape = Compound), size = size_point) +
-  labs(title = "PCA by Compound & Concentration",
-       x = x_lab,
-       y = y_lab) +
-  theme_pubr() +
-  theme(
-    plot.title = element_text(hjust = 0.5, size = size_title, face = "bold"),
-    axis.text = element_text(size = size_axis),
-    axis.title = element_text(size = size_axis),
-    legend.title = element_text(size = size_axis),
-    legend.text = element_text(size = size_axis),
-    panel.grid = element_blank(),
-    plot.margin = margin(10, 10, 10, 10)
-  ) +
-  scale_color_gradientn(
-    colours = rev(lighten(
-      viridis(100),
-      amount = 0.3
-    ))
-  ) +
-  theme(legend.position = "right")
-plots$p4
+plots$pca_compound_concentration <- plot_pca(
+  data,
+  colour_var = "Concentration",
+  shape_var = "Compound",
+  title_text = "PCA by Compound & Concentration",
+  legend_title = "Concentration",
+  colour_scale = scale_colour_gradientn(
+    colours = rev(lighten(viridis(100),
+                          amount = 0.3))
+  )
+)
+plots$pca_compound_concentration
 # p5: pc1 x pc2 by batch ####
-plots$p5 <- ggplot(data, aes(x = PC_1, y = PC_2)
-) +
-  geom_point(aes(color = Batch), size = size_point) +
-  labs(title = "PCA by Batch",
-       x = x_lab,
-       y = y_lab) +
-  theme_pubr() +
-  theme(
-    plot.title = element_text(hjust = 0.5, size = size_title, face = "bold"),
-    axis.text = element_text(size = size_axis),
-    axis.title = element_text(size = size_axis),
-    legend.title = element_text(size = size_axis),
-    legend.text = element_text(size = size_axis),
-    panel.grid = element_blank(),
-    plot.margin = margin(10, 10, 10, 10)
-  ) +
-  scale_color_manual(
+plots$pca_batch <- plot_pca(
+  data,
+  colour_var = "Batch",
+  title_text = "PCA by Batch",
+  legend_title = "Batch",
+  colour_scale = scale_colour_manual(
     values = pastel_cols
-  ) + 
-  theme(legend.position = "right")
-plots$p5
+  )
+)
+plots$pca_batch
+# create function to add fixed legend space ####
+add_fixed_legend_space <- function(plot,
+                                   plot_width = 1,
+                                   legend_width = 0.4) {
+  legend <- get_legend(
+    plot +
+      theme(
+        legend.position = "right"
+      )
+  )
+  plot_without_legend <- plot +
+    theme(
+      legend.position = "none"
+    )
+  plot_grid(
+    plot_without_legend,
+    legend,
+    nrow = 1,
+    rel_widths = c(
+      plot_width,
+      legend_width
+    )
+  )
+}
+# apply consistent legend space to plots ####
+plots_fixed <- map(
+  plots,
+  add_fixed_legend_space
+)
 # save plots ####
-ggsave(paste("outputs/figures/pca/", file_name, "pca_1.pdf"),
-       plots$p1,
-       width = plot_width,
-       height = plot_height,
-       units = "in",
-       dpi = 300)
-ggsave(paste("outputs/figures/pca/", file_name, "pca_2.pdf"),
-       plots$p2,
-       width = plot_width,
-       height = plot_height,
-       units = "in",
-       dpi = 300)
-ggsave(paste("outputs/figures/pca/", file_name, "pca_3.pdf"),
-       plots$p3,
-       width = plot_width,
-       height = plot_height,
-       units = "in",
-       dpi = 300)
-ggsave(paste("outputs/figures/pca/", file_name, "pca_4.pdf"),
-       plots$p4,
-       width = plot_width,
-       height = plot_height,
-       units = "in",
-       dpi = 300)
-ggsave(paste("outputs/figures/pca/", file_name, "pca_5.pdf"),
-       plots$p5,
-       width = plot_width,
-       height = plot_height,
-       units = "in",
-       dpi = 300)
+iwalk(
+  plots_fixed,
+  function(plot, plot_name) {
+    ggsave(
+      filename = paste0(
+        "outputs/figures/pca/",
+        file_name,
+        "_",
+        plot_name,
+        ".pdf"
+      ),
+      plot = plot,
+      width = 3.7,
+      height = 3,
+      units = "in",
+      dpi = 300
+    )
+  }
+)
 rm(list = ls())
