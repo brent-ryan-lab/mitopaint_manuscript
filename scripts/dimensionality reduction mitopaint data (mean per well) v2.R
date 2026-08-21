c7a084e200b43ffa9e435cb7124bd9049c2ad218
sarahnjfranks <snjfranks@gmail.com>
2026-08-21 11:47:11 +0100

    update dimensionality reduction mitopaint data (mean per well) v1 to save neighbour graphs, and rename file to v2 to reflect change

--- scripts/dimensionality reduction mitopaint data (mean per well) v2.R
+++ scripts/dimensionality reduction mitopaint data (mean per well) v2.R
@@ -1,9 +1,9 @@
-# Title: dimensionality reduction mitopaint data (mean per well) v1
+# Title: dimensionality reduction mitopaint data (mean per well) v2
 # Step: 6
 # R: 4.4.1
 # Author: Sarah Franks
 # Project: mitopaint manuscript
-# Last edit: 18-06-2026
+# Last edit: 21-06-2026
 
 # load packages ####
 library(data.table)
@@ -99,8 +99,13 @@ df.seurat <- FindNeighbors(
   # k_param dictates how many nearest neighbors each point should have
   # this is dependent of number of technical well replicates in experiment
   k.param = k_param,
-  reduction = "pca"
+  reduction = "pca",
+  graph.name = c("pca_nn", "pca_snn")
 )
+# save nn graphs in graphs
+graphs <- list()
+graphs$pca_nn <- df.seurat@graphs[["pca_nn"]]
+graphs$pca_snn <- df.seurat@graphs[["pca_snn"]]
 # find pca clusters for seurat, based on pca nearest neighbors
 df.seurat <- FindClusters(
   df.seurat,
@@ -108,6 +113,7 @@ df.seurat <- FindClusters(
   # higher res = more clusters, lower res = less clusters
   # this is tuneable based on how many subpopulations are expected
   resolution = res,
+  graph.name = "pca_snn",
   cluster.name = "PCA_NN"
 )
 # store pca embeddings (row names = wells, col names = PCs) in a dataframe
@@ -229,8 +235,12 @@ df.seurat <- FindNeighbors(
   # k_param dictates how many nearest neighbors each point should have
   # this is dependent of number of technical well replicates in experiment
   k.param = k_param,
-  reduction = "tsne"
+  reduction = "tsne",
+  graph.name = c("tsne_nn", "tsne_snn")
 )
+# save nn graphs in graphs
+graphs$tsne_nn <- df.seurat@graphs[["tsne_nn"]]
+graphs$tsne_snn <- df.seurat@graphs[["tsne_snn"]]
 # find tsne clusters for seurat, based on tsne nearest neighbors
 df.seurat <- FindClusters(
   df.seurat,
@@ -238,6 +248,7 @@ df.seurat <- FindClusters(
   # higher res = more clusters, lower res = less clusters
   # this is tuneable based on how many subpopulations are expected
   resolution = res,
+  graph.name = "tsne_snn",
   cluster.name = "tSNE_NN"
 )
 # store tsne embeddings (row names = wells, col names = tsne) in a dataframe
@@ -277,8 +288,12 @@ df.seurat <- FindNeighbors(
   # k_param dictates how many nearest neighbors each point should have
   # this is dependent of number of technical well replicates in experiment
   k.param = k_param,
-  reduction = "umap"
+  reduction = "umap",
+  graph.name = c("umap_nn", "umap_snn")
 )
+# save nn graphs in graphs
+graphs$umap_nn <- df.seurat@graphs[["umap_nn"]]
+graphs$umap_snn <- df.seurat@graphs[["umap_snn"]]
 # find umap clusters for seurat, based on umap nearest neighbors
 df.seurat <- FindClusters(
   df.seurat,
@@ -286,6 +301,7 @@ df.seurat <- FindClusters(
   # higher res = more clusters, lower res = less clusters
   # this is tuneable based on how many subpopulations are expected
   resolution = res,
+  graph.name = "umap_snn",
   cluster.name = "UMAP_NN"
 )
 # store umap embeddings (row names = wells, col names = umap) in a dataframe
@@ -332,4 +348,22 @@ write.csv(umap$embeddings,
           paste(
             "data/processed/", file_name, "_", integrate_state, "_", redu_state, "_umap_embeddings.csv", sep = "")
 )
+# save nn and snn edge list
+dir.create("data/processed/graphs", recursive = TRUE, showWarnings = FALSE)
+iwalk(graphs, function(g, graph_name) {
+  edge_df <- Matrix::summary(g)
+  edge_df$from <- rownames(g)[edge_df$i]
+  edge_df$to <- colnames(g)[edge_df$j]
+  write.csv(
+    edge_df[, c("from", "to", "x")],
+    paste0(
+      "data/processed/",
+      file_name, "_",
+      integrate_state, "_",
+      redu_state, "_",
+      graph_name, "_edges.csv"
+    ),
+    row.names = FALSE
+  )
+})
 rm(list = ls())
